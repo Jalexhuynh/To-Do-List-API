c695b8eb3b18505283d0b673a4c598db25887ab2
Justin Huynh <j.alexhuynh@gmail.com>
2018-08-20 12:08:48 -0700

    Organize route helpers and add static directory.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require("express"),
-	app		= express(),
-	port	= process.env.PORT || 3000;
-	bodyParser = require("body-parser");
+var express 	= require("express"),
+	app			= express(),
+	port		= process.env.PORT || 3000,
+	bodyParser  = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/views"));
 
 var todoRoutes = require("./routes/todos.js")
 
@@ -13,7 +14,7 @@ var todoRoutes = require("./routes/todos.js")
 // is passed in to json.
 
 app.get("/", function(req, res) {
-	res.send("Hello from the root route");
+	res.sendFile("index.html");
 });
 
 app.use("/api/todos", todoRoutes);
